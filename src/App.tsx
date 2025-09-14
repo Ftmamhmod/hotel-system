945f54f612f958a2a940582739458d9d4588d331
Rawan Ahmed <RawanSadek267@gmail.com>
2025-09-15 01:46:09 +0300

    feat: implement routing and layout structure for the hotel system application

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,73 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LandingPage from "./Modules/Shared/LandingPage/LandingPage";
+import NotFound from "./Modules/Shared/NotFound/NotFound";
+import AuthLayout from "./Modules/Shared/AuthLayout/AuthLayout";
+import Register from "./Modules/AuthModule/Register/Register";
+import ForgotPassword from "./Modules/AuthModule/ForgotPassword/ForgotPassword";
+import ResetPassword from "./Modules/AuthModule/ResetPassword/ResetPassword";
+import ChangePassword from "./Modules/AuthModule/ChangePassword/ChangePassword";
+import MasterLayout from "./Modules/Shared/MasterLayout/MasterLayout";
+import Login from "./Modules/AuthModule/Login/Login";
+import AdminLayout from "./Modules/Shared/AdminLayout/AdminLayout";
+import RoomDetails from "./Modules/UserModule/RoomDetails/RoomDetails";
+import Explore from "./Modules/UserModule/Explore/Explore";
+import Favourites from "./Modules/UserModule/Favourites/Favourites";
+import AdminDashboard from "./Modules/AdminModule/AdminDashboard/AdminDashboard";
+import RoomsList from "./Modules/AdminModule/Rooms/RoomsList";
+import RoomData from "./Modules/AdminModule/Rooms/RoomData";
+import FacilitiesList from "./Modules/AdminModule/Facilities/FacilitiesList";
+import ADSList from "./Modules/AdminModule/ADS/ADSList";
+import BookingList from "./Modules/AdminModule/Booking/BookingList";
 
 function App() {
-  const [count, setCount] = useState(0);
+const routes = createBrowserRouter([
+    {
+      path:'',
+      element: <MasterLayout/>,
+      errorElement: <NotFound/>,
+      children: [
+        {index: true, element: <LandingPage/>},
+        {path: 'landing-page', element: <LandingPage/>},
+        {path: 'room-details', element: <RoomDetails/>},
+        {path: 'explore', element: <Explore/>},
+        {path: 'favourits', element: <Favourites/>},
+      ]
+    },
+    {
+      path: "",
+      element: <AuthLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forget-password", element: <ForgotPassword /> },
+        { path: "reset-password", element: <ResetPassword /> },
+        { path: "change-password", element: <ChangePassword />},
+      ],
+    },
 
+    {
+      path:'dashboard',
+      element: <AdminLayout/>,
+      errorElement: <NotFound/>,
+      children: [
+        {index: true, element: <AdminDashboard/>},
+        {path: 'rooms', element: <RoomsList/>},
+        {path: 'add-room', element: <RoomData/>},
+        {path: 'edit-room/:id', element: <RoomData/>},
+        {path: 'view-room/:id', element: <RoomData/>},
+        {path: 'facilities', element: <FacilitiesList/>},
+        {path: 'ads', element: <ADSList/>},
+        {path: 'booking-list', element: <BookingList/>},
+      ]
+    },
+
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ToastContainer/>
+      <RouterProvider router={routes}></RouterProvider>
     </>
   );
 }
--- src/Modules/Shared/AdminLayout/AdminLayout.tsx
+++ src/Modules/Shared/AdminLayout/AdminLayout.tsx
@@ -1,8 +1,9 @@
+import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   return (
     <div>
-      AdminLayout
+      <Outlet/>
     </div>
   )
 }
