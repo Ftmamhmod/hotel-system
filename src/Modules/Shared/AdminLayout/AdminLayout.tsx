c58b0503aef969f42699e77173e563c755f3966d
ftmamhmod <ftmamhmod2@gmail.com>
2025-09-18 11:48:37 +0300

    feat: update navigation logic in Login component and improve ResetPassword input IDs

--- src/Modules/Shared/AdminLayout/AdminLayout.tsx
+++ src/Modules/Shared/AdminLayout/AdminLayout.tsx
@@ -1,9 +1,21 @@
+import Box from "@mui/material/Box";
 import { Outlet } from "react-router-dom";
+import AdminNavbar from "./../AdminNavbar/AdminNavbar";
+import AdminSidebar from "../AdminSidebar/AdminSidebar";
 
 export default function AdminLayout() {
   return (
-    <div>
-      <Outlet/>
-    </div>
-  )
+    <Box component={"div"} sx={{ display: "flex" }}>
+      <Box
+        component={"nav"}
+        sx={{ width: "240px", bgcolor: "#f5f5f5", minHeight: "100vh" }}
+      >
+        <AdminSidebar />
+      </Box>
+      <Box component={"div"} sx={{ flexGrow: 1, p: 3 }}>
+        <AdminNavbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 }
