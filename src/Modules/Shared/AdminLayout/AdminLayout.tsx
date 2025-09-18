98ebbecd29186e6dc4c6a54fbc923b4f87ae3abf
ftmamhmod <ftmamhmod2@gmail.com>
2025-09-18 12:18:36 +0300

    feat: integrate i18next for multilingual support and add Arabic translations

--- src/Modules/Shared/AdminLayout/AdminLayout.tsx
+++ src/Modules/Shared/AdminLayout/AdminLayout.tsx
@@ -1,9 +1,16 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import { Outlet } from "react-router-dom";
 import AdminNavbar from "./../AdminNavbar/AdminNavbar";
 import AdminSidebar from "../AdminSidebar/AdminSidebar";
+import { useTranslation } from "react-i18next";
+import "../../../i18n/i18n";
 
 export default function AdminLayout() {
+  const { i18n } = useTranslation();
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === "ar" ? "en" : "ar");
+  };
   return (
     <Box component={"div"} sx={{ display: "flex" }}>
       <Box
@@ -13,6 +20,11 @@ export default function AdminLayout() {
         <AdminSidebar />
       </Box>
       <Box component={"div"} sx={{ flexGrow: 1, p: 3 }}>
+        <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
+          <Button variant="outlined" onClick={toggleLang}>
+            {i18n.language === "ar" ? "English" : "العربية"}
+          </Button>
+        </Box>
         <AdminNavbar />
         <Outlet />
       </Box>
